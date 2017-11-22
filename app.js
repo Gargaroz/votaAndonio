25f65c3e03faaf8f43b7f6b79a9561a5d6ebf22e
Gargaroz <g.esposito16@gmail.com>
2017-11-22 07:48:39 +0100

    app skeleton via yo cg-angular

---
+++ app.js
@@ -0,0 +1,23 @@
+angular.module('votaAndonio', ['ui.bootstrap','ngRoute','ngAnimate']);
+
+angular.module('votaAndonio').config(function($routeProvider) {
+
+    /* Add New Routes Above */
+    $routeProvider.otherwise({redirectTo:'/home'});
+
+});
+
+angular.module('votaAndonio').run(function($rootScope) {
+
+    $rootScope.safeApply = function(fn) {
+        var phase = $rootScope.$$phase;
+        if (phase === '$apply' || phase === '$digest') {
+            if (fn && (typeof(fn) === 'function')) {
+                fn();
+            }
+        } else {
+            this.$apply(fn);
+        }
+    };
+
+});
